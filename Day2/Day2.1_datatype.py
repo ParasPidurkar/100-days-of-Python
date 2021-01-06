337c4d71f21b55c7c963aad51679582641390368
ParasPidurkar <45849288+ParasPidurkar@users.noreply.github.com>
2021-01-06 23:14:31 +0530

    Day2

---
+++ Day2/Day2.1_datatype.py
@@ -0,0 +1,13 @@
+#datatype
+#print(len(12345))
+print("Hello"[0]) #subscripting
+print(123+345)
+print("123"+"456")
+print(123_456_789)
+#float
+print(3.1415)
+#boolean
+a= False
+print(a)
+#b=true
+#print (b)      error as the boolean value shouldhave first letter capital
